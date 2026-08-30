f31e33a3c6e775ff634182d0880c0ace30f4ccf9
HUSSEIN <167720962+ihhaiq@users.noreply.github.com>
2026-08-30 22:03:23 +0300

    Register ready Math Rich Message router

--- app/routers/rich_editor.py
+++ app/routers/rich_editor.py
@@ -6,6 +6,7 @@ from app.routers import editor_core
 from app.routers.block_preview import router as block_preview_router
 from app.routers.button_guide import install_into as install_button_guide
 from app.routers.developer import router as developer_router
+from app.routers.math_ready import router as math_ready_router
 from app.routers.media_events import router as media_events_router
 from app.routers.miniapp import router as miniapp_router
 
@@ -27,6 +28,7 @@ router.include_router(developer_router)
 router.include_router(miniapp_router)
 router.include_router(block_preview_router)
 router.include_router(media_events_router)
+router.include_router(math_ready_router)
 router.include_router(editor_core_router)
 
 __all__ = ["router"]
